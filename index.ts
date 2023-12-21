94f8c936ffe3a70c981136f4eb95b4c587cc1420
Edson Carli <gandnegro@gmail.com>
2023-12-21 16:16:48 +0000

    Start commit

---
+++ index.ts
@@ -0,0 +1,24 @@
+import { Provider,ServerData } from "taulukko-messages";
+
+export class RabbitProvider implements Provider{
+    constructor(){}
+    async open():Promise<any>{
+        throw new Error("Not implemented yet");
+    }
+    async close():Promise<any>{
+        throw new Error("Not implemented yet");
+    }
+    async forceClose():Promise<any>{}
+    data():ServerData{
+        throw new Error("Not implemented yet");
+    }
+    publishers():Array<any>{
+        throw new Error("Not implemented yet");
+    }
+    subscribers():Array<any>{
+        throw new Error("Not implemented yet");
+    }
+    async sendAll(topic:string,data:any){
+        throw new Error("Not implemented yet");
+    }    
+}
